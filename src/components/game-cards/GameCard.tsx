0c88f7a2bf559034a1225765293723af773a67d7
Ahmed A Sulaiman <132774621+Ahmed-Sulaiman@users.noreply.github.com>
2024-08-05 20:20:55 -0400

    adding Genre headings

--- src/components/game-cards/GameCard.tsx
+++ src/components/game-cards/GameCard.tsx
@@ -13,13 +13,20 @@ const GameCard = ({ game }: props) => {
     <Card minHeight={{ base: "300px", md: "3500px", lg: "350px" }}>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize={"2xl"}>{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading
+          fontSize="2xl"
+          overflow="hidden"
+          textOverflow="ellipsis"
+          whiteSpace="nowrap"
+        >
+          {game.name}
+        </Heading>
       </CardBody>
     </Card>
   );
